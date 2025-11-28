364f72ef0d278dc0069bb2c1aedd02e723b9263d
hayashi-dtp <hayashi@dtpnet.jp>
2025-11-28 15:16:39 +0900

    Update MasterMeishoViewModel.cs
    test

--- CvnetClient/ViewModels/v01Master/MasterMeishoViewModel.cs
+++ CvnetClient/ViewModels/v01Master/MasterMeishoViewModel.cs
@@ -27,7 +27,7 @@ using System.Windows;
 using System.Windows.Controls;
 
 namespace CvnetClient.ViewModels {
-	public partial class MasterMeishoViewModel : BaseViewModel {
+	public partial class MasterMeishoViewModel123 : BaseViewModel {
 		[ObservableProperty]
 		List<string>? listKubun;
 		[ObservableProperty]
